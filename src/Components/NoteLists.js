075964bc525f5a9398a66f4060107f71db7076c5
FrostyyOPP <hackrs1208@gmail.com>
2022-04-08 12:39:03 +0530

    first commit

---
+++ src/Components/NoteLists.js
@@ -0,0 +1,19 @@
+import AddNote from './AddNote';
+import Note from './Note'
+
+const NoteLists = ({ notes, handleAddNote, handleDeleteNote }) =>{
+    return( 
+        <div className='notes-list'>
+         {notes.map((note) => 
+          <Note 
+          id={note.id} 
+          text={note.text} 
+          date={note.date} 
+          handleDeleteNote={handleDeleteNote} 
+          />
+          )}  
+          <AddNote handleAddNote={handleAddNote}/>
+        </div>
+    )
+}
+export default NoteLists;
